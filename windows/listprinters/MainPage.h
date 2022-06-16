c34950a458db2c9ebbb2e268a42dfc855cdf766c
Carlos <ixarlos@gmail.com>
2022-06-16 13:32:29 -0400

    first commit

---
+++ windows/listprinters/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::listprinters::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::listprinters::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
